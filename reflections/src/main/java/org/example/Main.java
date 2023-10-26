6cde549480c55acaa163deb85e134450c9b4f635
adam urbanowicz <adam.urbanow@gmail.com>
2023-10-26 12:03:15 +0200

    w4 2023

--- reflections/src/main/java/org/example/Main.java
+++ reflections/src/main/java/org/example/Main.java
@@ -13,108 +13,160 @@ public class Main {
         var employee = new Employee();
 //        funWithTypes(employee.getClass());
 //        funWithConstructors(employee.getClass());
-        funWithMethods(employee.getClass());
+//        funWithMethods(employee.getClass());
     }
 
 
 
     private static void funWithTypes(Class<?> clazz) {
 
-        String className = clazz
-                //.getSimpleName()
-                //.getName()
-                .getPackageName()
-                ;
+//        String className = clazz
+//                //.getSimpleName()
+//                //.getName()
+//                .getPackageName()
+//                ;
+//
+//        System.out.println(className);
 
-        System.out.println(className);
 
-        String superClassName = clazz.getSuperclass()
-                //.getSimpleName()
-                //.getName()
-                .getPackageName()
-        ;
+        /**
+         * Superklasa
+         */
+//        String superClassName = clazz.getSuperclass()
+//                //.getSimpleName()
+//                //.getName()
+//                .getPackageName()
+//        ;
+//
+//        System.out.println(superClassName);
 
-        System.out.println(superClassName);
+        /**
+         * Interfejsy
+         */
+//        var interfaces = Arrays.stream(clazz.getInterfaces()).toList();
+//
+//        interfaces.forEach(x->
+//                      System.out.println(x.getName())
+//                );
 
-        var interfaces = Arrays.stream(clazz.getInterfaces()).toList();
-
-        interfaces.forEach(x->
-                      System.out.println(x.getName())
-                );
-
-        var superClassInterfaces = Arrays.stream(clazz.getSuperclass().getInterfaces()).toList();
-
-        superClassInterfaces.forEach(x->
-                        System.out.println(x.getName())
-                );
+        /**
+         * Interfejsy super klasy
+         */
+//        var superClassInterfaces = Arrays.stream(clazz.getSuperclass().getInterfaces()).toList();
+//
+//        superClassInterfaces.forEach(x->
+//                        System.out.println(x.getName())
+//                );
     }
 
     private static void funWithConstructors(Class<?> clazz) {
 
-        var constructors = Arrays.stream(clazz.getConstructors()).toList();
-        message("konstruktory klasy Employee");
-        constructors.forEach(constructor->{
-                    var name = constructor.getName();
-                    var arguments = constructor.getParameters();
-                    var modifiers = constructor.getModifiers();
-                    System.out.println(Modifier.toString(modifiers) + " " + name +" has " +constructor.getParameterCount() +" parameters");
-                });
+        /**
+         * Pobieranie konstruktorów
+         */
 
-        var employees = constructors.stream()
-                .map(constructor-> {
-                    try {
-                        if(constructor.getParameterCount()==1)
-                            return constructor.newInstance(100);
-                    } catch (Exception e) {
-                    }
-                    return null;
-                }).filter(x->x!=null).toList();
-        System.out.println(employees.get(0));
+//        var constructors = Arrays.stream(clazz.getConstructors()).toList();
+//        message("konstruktory klasy Employee");
+
+        /**
+         * badanie modyfikatorów, parametrów oraqz nazwy
+         */
+
+//        constructors.forEach(constructor->{
+//                    var name = constructor.getName();
+//                    var arguments = constructor.getParameters();
+//                    var modifiers = constructor.getModifiers();
+//                    System.out.println(Modifier.toString(modifiers) + " " + name +" has " +constructor.getParameterCount() +" parameters");
+//                });
+
+        /**
+         * Przykład tworzenia obiektów
+         */
+
+//        var employees = constructors.stream()
+//                .map(constructor-> {
+//                    try {
+//                        if(constructor.getParameterCount()==1)
+//                            return constructor.newInstance(100);
+//                    } catch (Exception e) {
+//                    }
+//                    return null;
+//                }).filter(x->x!=null).toList();
+//        System.out.println(employees.get(0));
     }
     private static void funWithMethods(Class<?> clazz) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
 
-        message("Wszystkie metody klasy Employee (także metody klas nadrzędnych)");
-        var methodList = Arrays.stream(clazz.getMethods()).toList();
+        /**
+         * pobieranie wszystkich metod
+         */
+//        message("Wszystkie metody klasy Employee (także metody klas nadrzędnych)");
+//        var methodList = Arrays.stream(clazz.getMethods()).toList();
 
-        methodList.forEach(
-                method->{
-                    var name = method.getName();
-                    var modifier = method.getModifiers();
-                    var modifierInString = Modifier.toString(modifier);
-                    var parameters = method.getParameterTypes();
-                    System.out.println(modifierInString +" " + name + " has " + method.getParameterCount()+ " parameters");
-                }
-        );
+        /**
+         * sprawdzanie sygnatury
+         */
 
-        var tmp = new Employee();
-        message("Metody z klasy Employee");
-        var declaredMethods = Arrays.stream(clazz.getDeclaredMethods());
-        declaredMethods.forEach(
-                method->{
-                    var name = method.getName();
-                    var modifier = method.getModifiers();
-                    var modifierInString = Modifier.toString(modifier);
-                    var parameters = method.getParameterTypes();
-                    System.out.println(modifierInString +" " + name + " has " + method.getParameterCount()+ " parameters");
-                }
-        );
+//        methodList.forEach(
+//                method->{
+//                    var name = method.getName();
+//                    var modifier = method.getModifiers();
+//                    var modifierInString = Modifier.toString(modifier);
+//                    var parameters = method.getParameterTypes();
+//                    System.out.println(modifierInString +" " + name + " has " + method.getParameterCount()+ " parameters");
+//                }
+//        );
 
-        message("Test działania/wywoływania metod");
-        var employee = new Employee("jan", "nowak", 5000);
+        /**
+         * pobranie metod zadeklarowanych tylko w danej klasie (omijane sa metody z superklas)
+         */
 
-        var printMethod = clazz.getMethod("printDetails");
-        printMethod.invoke(employee);
-        var setIdMethod = clazz.getDeclaredMethod("setIncome", double.class);
-        setIdMethod.invoke(employee, 1200);
-        var getIdMethod = clazz.getDeclaredMethod("getIncome");
-        System.out.println("nowa pensja: " + getIdMethod.invoke(employee));
+//        var tmp = new Employee();
+//        message("Metody z klasy Employee");
+//        var declaredMethods = Arrays.stream(clazz.getDeclaredMethods());
 
-        message("wywoływanie metody prywatnej");
-        //employee.secretAction(); // nie można tak !!
+        /**
+         * tak jak wyzej sprawdzanie sygnatur i parametrów metod
+         */
 
-        var secretMethod = clazz.getDeclaredMethod("secretAction");
-        secretMethod.setAccessible(true);
-        secretMethod.invoke(employee);
+//        declaredMethods.forEach(
+//                method->{
+//                    var name = method.getName();
+//                    var modifier = method.getModifiers();
+//                    var modifierInString = Modifier.toString(modifier);
+//                    var parameters = method.getParameterTypes();
+//                    System.out.println(modifierInString +" " + name + " has " + method.getParameterCount()+ " parameters");
+//                }
+//        );
+//
+        /**
+         * wywoływanie metod
+         */
+
+//        message("Test działania/wywoływania metod");
+//        var employee = new Employee("jan", "nowak", 5000);
+//
+//        var printMethod = clazz.getMethod("printDetails");
+//        printMethod.invoke(employee);
+
+        /**
+         * wywołanie getterów i setterów
+         */
+
+//        var setIdMethod = clazz.getDeclaredMethod("setIncome", double.class);
+//        setIdMethod.invoke(employee, 1200);
+//        var getIdMethod = clazz.getDeclaredMethod("getIncome");
+//        System.out.println("nowa pensja: " + getIdMethod.invoke(employee));
+//
+//        message("wywoływanie metody prywatnej");
+//        //employee.secretAction(); // nie można tak !!
+
+        /**
+         * wywołanie metod prywatnych
+         */
+
+//        var secretMethod = clazz.getDeclaredMethod("secretAction");
+//        secretMethod.setAccessible(true);
+//        secretMethod.invoke(employee);
     }
 
     public static void message(String message){
